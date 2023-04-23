91598a951f072aa2b69abeb29c6a4d8e2e6cd471
SSG <gssenyonjo@gmail.com>
2023-04-23 08:41:14 +0300

    added MusicStoreContext to services in Program.cs

--- Program.cs
+++ Program.cs
@@ -1,9 +1,17 @@
+using HelloMySql.Models;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 
+// see https://www.c-sharpcorner.com/article/how-to-connect-mysql-with-asp-net-core/
+builder.Services.Add(new ServiceDescriptor(typeof(MusicStoreContext), new MusicStoreContext(builder.Configuration.GetConnectionString("DefaultConnection"))));
+
+
 var app = builder.Build();
+
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
